8c3c77b25336c7667d44a780ace6de5ec3571448
Javad0032 <javad.saboori@gritacademy.se>
2025-03-11 08:59:51 +0100

    Add files via upload

---
+++ Portfilo/script.js
@@ -0,0 +1,14 @@
+document.getElementById('number').innerHTML=Boolean(10!==9);
+let x=true
+Boolean.prototype.myColor=
+function(){
+    if (this .valueOf()==true){
+        return'green';
+    }
+    else{
+        return 'red';
+    }
+};
+let a=false;
+let b=true;
+document.getElementById('demo').innerHTML=a.myColor();
